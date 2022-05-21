217d3f9771870b7a3dc632e9ef45a8ff0eef1da6
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-05-21 11:27:03 +0100

    news

--- src/test/java/org/imageconverter/batch/SplitFileStepHappyPathTest.java
+++ src/test/java/org/imageconverter/batch/SplitFileStepHappyPathTest.java
@@ -1,12 +1,14 @@
 package org.imageconverter.batch;
 
 import static java.io.File.separator;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ONE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.imageconverter.config.BatchConfiguration.*;
+import static org.imageconverter.config.BatchConfiguration.SPLIT_FILE_STEP;
 import static org.springframework.batch.core.ExitStatus.COMPLETED;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.imageconverter.batch.step02splitfile.SplitFileStepConfiguration;
@@ -61,6 +63,14 @@ class SplitFileStepHappyPathTest extends AbstractBatchTest {
 	jobRepositoryTestUtils = new JobRepositoryTestUtils(jobRepository, batchDataSource);
 
 	createBatchFile();
+	
+	final var inputFolderAbsolutePath = Paths.get(inputFolder.getURI());
+	final var processingAbsolutePath = Paths.get(processingFolder.getURI());
+	Files.move(//
+			Paths.get(inputFolderAbsolutePath.toString() + separator + fileName), //
+			Paths.get(processingAbsolutePath.toString() + separator + fileName), //
+			REPLACE_EXISTING //
+	);	
     }
 
     @AfterAll
